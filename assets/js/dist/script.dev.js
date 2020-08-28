82ca89362a462eb4db7bcbcd4353c5dbf4198dc2
Nasib <bduti09@gmail.com>
2020-08-28 16:51:32 +0400

    day 6

---
+++ assets/js/dist/script.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var count = 1;
+
+while (count <= 10) {
+  console.log(count);
+  count++;
+}
---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let count = 1;
+
+while (count <= 10) {
+  console.log(count);
+  count++;
+}
+
+for (count = 1; count <= 10; count++) {
+  console.log(count)
+
+}
